c87093f25218c837f4a41c920f57a85d179a7499
LastyW <100523859+Lasty120@users.noreply.github.com>
2026-06-05 15:05:18 +0500

    Changed .env parsing in config

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ import pytz
 load_dotenv()
 
 TOKEN = getenv("TG_BOT_TOKEN")
-OPENAI_API_KEY = getenv("GOOGLE_API_KEY") # Заменил на гугл вместо OPENAI_API_KEY для тестов
-OPENAI_DEFAULT_MODEL = getenv("GOOGLE_DEFAULT_MODEL")
-OPENAI_DEFAULT_URL = getenv("GOOGLE_DEFAULT_URL")
+OPENAI_API_KEY = getenv("OPENAI_API_KEY")
+OPENAI_DEFAULT_MODEL = getenv("OPENAI_DEFAULT_MODEL")
+OPENAI_DEFAULT_URL = getenv("OPENAI_DEFAULT_URL")
 DB_PATH = "bot.db"
 GROQ_API_KEY = getenv("GROQ_API_KEY")
 
